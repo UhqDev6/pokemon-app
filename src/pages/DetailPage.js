309dce3d52089d5455f7b85e9461469827f0a9cf
M Dhiya Ulhaq <uhqdev@192.168.1.16>
2023-03-03 21:52:47 +0800

    initial fitur detailPage

--- src/pages/DetailPage.js
+++ src/pages/DetailPage.js
@@ -1,9 +1,22 @@
-import React from 'react';
+/* eslint-disable max-len */
+import React, { useState } from 'react';
+import { useParams } from 'react-router-dom';
+import SpinnerLoading from '../components/atoms/SpinnerLoading';
 
 function DetailPage() {
+  const { id } = useParams();
+  const [isLoading, setIsloading] = useState(false);
   return (
-    <div>
-      detail
+    <div className="p-5 flex w-full mx-auto justify-center sm:justify-start">
+      <div className="flex flex-wrap gap-5 justify-center">
+        {
+          isLoading ? (
+            <SpinnerLoading />
+          ) : (
+            <p>{id}</p>
+          )
+        }
+      </div>
     </div>
   );
 }
