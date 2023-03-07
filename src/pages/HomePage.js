3e7a2c8402c6f7beebc0a57828f5f30aaf080f57
M Dhiya Ulhaq <uhqdev@192.168.1.16>
2023-03-07 17:34:54 +0800

    added search feature & fix is component

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { Link, useNavigate } from 'react-router-dom';
+import { Link, useNavigate, useSearchParams } from 'react-router-dom';
 import { BsFillBookmarkHeartFill, BsSliders, BsXLg } from 'react-icons/bs';
 import Card from '../components/atoms/Card';
 import SpinnerLoading from '../components/atoms/SpinnerLoading';
@@ -9,6 +9,8 @@ import { asyncAddToFavorite } from '../states/favorite/action';
 import api from '../utils/api';
 import FilteredBar from '../components/atoms/FilteredBar';
 import Wrapper from '../components/atoms/Wrapper';
+import Search from '../components/atoms/Search';
+import useInput from '../hooks/useInput';
 
 function HomePage() {
   const {
@@ -18,46 +20,62 @@ function HomePage() {
   const navigate = useNavigate();
   const dispatch = useDispatch();
   const [offset, setOffSet] = useState(0);
-  const [poke, setPoke] = useState(pokemons);
+  const [poke, setPoke] = useState([]);
   const [active, setActive] = useState(false);
+  const [searchParams, setSearchParams] = useSearchParams();
+  const [searching, setSearching] = useInput(() => searchParams.get('search') || '');
 
   const handleAddFavorite = (key) => {
     dispatch(asyncAddToFavorite(key));
     navigate('/favorite');
   };
 
+  const onSearchChangeHandler = (search) => {
+    setSearching(search);
+    setSearchParams({ search });
+  };
+
   const handleLoadData = async () => {
     const results = await api.getAllPokemon(offset);
     setPoke([...poke, ...results]);
     setOffSet(offset + 10);
+    setIsloading(false);
   };
+
+  // eslint-disable-next-line max-len
+  const getPokemon = poke.filter((item) => item.name.toString().toLowerCase().includes(searching.toLowerCase()));
 
   useEffect(() => {
     handleLoadData();
-    setIsloading(false);
-  }, [dispatch]);
+  }, []);
+
   return (
     <Wrapper>
       <div className="flex justify-end mr-10 gap-5">
-        <Link to="/favorite">
-          <div className="flex-none">
-            <BsFillBookmarkHeartFill color="#9f60c5" />
-          </div>
-        </Link>
-        <button
-          type="button"
-          onClick={() => setActive(!active)}
-        >
-          <div className="flex-none">
-            {
-              !active ? (
-                <BsSliders color="#9f60c5" />
-              ) : (
-                <BsXLg color="#9f60c5" />
-              )
-            }
-          </div>
-        </button>
+        <div className="w-full ml-10">
+          <Search search={searching} searchChange={onSearchChangeHandler} />
+        </div>
+        <div className="">
+          <Link to="/favorite">
+            <div className="flex-none">
+              <BsFillBookmarkHeartFill color="#9f60c5" />
+            </div>
+          </Link>
+          <button
+            type="button"
+            onClick={() => setActive(!active)}
+          >
+            <div className="flex-none">
+              {
+                !active ? (
+                  <BsSliders color="#9f60c5" />
+                ) : (
+                  <BsXLg color="#9f60c5" />
+                )
+              }
+            </div>
+          </button>
+        </div>
       </div>
       <div className={active ? 'block' : 'hidden'}>
         <FilteredBar />
@@ -65,10 +83,11 @@ function HomePage() {
       <div className="p-5 flex w-full mx-auto justify-center sm:justify-start">
         <div className="flex flex-wrap gap-5 justify-center">
           {
+          // eslint-disable-next-line no-nested-ternary
           isLoading ? (
             <SpinnerLoading />
-          ) : (
-            poke?.map((pokemon) => (
+          ) : getPokemon.length > 0
+            ? getPokemon?.map((pokemon) => (
               <div className="md=flex md:flex-row justify-center" key={pokemon.id}>
                 <Card pokemon={pokemon}>
                   <PokemonList pokemon={pokemon} key={pokemon.id} isLoading={isLoading} />
@@ -84,12 +103,12 @@ function HomePage() {
                 </div>
               </div>
             ))
-          )
+            : <p className="text-xs font-light text-slate-500 mt-40">Pokemon empty...</p>
         }
         </div>
       </div>
       {
-        !isLoading && (
+        getPokemon.length > 0 && (
           <div className="flex w-full">
             <button
               type="button"
